c7f81e21483df763fae7a4d2f32cef3ec6e0df70
Snezana <icegirl-13@hotmail.com>
2017-02-03 14:55:23 +0000

    Check xml added

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -4,7 +4,18 @@
 
     <div class="container">
         <div class="content">
+            <div>
+                @if(isset($error)) 
+                
+               <p class="alert-danger">{{$error}}</p>
+                
+                @endif
+            </div>
+            
+             @if(isset($name,$value,$unit))
             <div class="title">Current forecast is {{$name}} with {{$value . " " . $unit}} temperature</div>
+            
+            @endif
            </div>
         </div>
         
